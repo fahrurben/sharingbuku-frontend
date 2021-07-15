39fe635f959d89a93497759c5a7f28dc93902ec3
Fahrur Razi <fahrurben@gmail.com>
2021-07-15 16:08:53 +0700

    Add profile menu in header

--- src/pages/layouts/default.js
+++ src/pages/layouts/default.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory, Link } from 'react-router-dom';
 import LoadingOverlay from 'react-loading-overlay';
@@ -6,6 +6,8 @@ import BounceLoader from 'react-spinners/BounceLoader';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { AUTH_FULL_NAME_KEY, AUTH_TOKEN_KEY } from '../../constant';
+import clsx from 'clsx';
+import _ from 'lodash';
 
 export default function DefaultLayout({ children }) {
   const { t, i18n } = useTranslation();
@@ -13,6 +15,28 @@ export default function DefaultLayout({ children }) {
   const error = useSelector((state) => state.global.error);
   const history = useHistory();
   const fullName = localStorage.getItem(AUTH_FULL_NAME_KEY);
+
+  const [showProfile, setShowProfile] = React.useState(false);
+
+  function toggleProfile() {
+    setShowProfile(!showProfile);
+  }
+
+  useEffect(() => {
+    const pageClickEvent = (e) => {
+      setShowProfile(!showProfile);
+    };
+
+    // If the item is active (ie open) then listen for clicks
+    if (showProfile) {
+      window.addEventListener('click', pageClickEvent);
+    }
+
+    return () => {
+      window.removeEventListener('click', pageClickEvent);
+    }
+
+  }, [showProfile]);
 
   function logoutClicked() {
     localStorage.removeItem(AUTH_TOKEN_KEY);
@@ -23,7 +47,7 @@ export default function DefaultLayout({ children }) {
   return (
     <LoadingOverlay
       active={isLoading}
-      spinner={<BounceLoader />}
+      spinner={<BounceLoader/>}
     >
       <nav className="block w-full h-auto sm:h-16 bg-indigo-900">
         <div className="container mx-auto h-full">
@@ -33,23 +57,37 @@ export default function DefaultLayout({ children }) {
               <div className="md:ml-8">
                 <input
                   className="float-left appearance-none md:w-80 ml-2 block bg-gray-50 text-gray-700 border border-gray-500 rounded rounded-r-none py-2 px-2 h-10 border-r-0"
-                  id="" type="text" placeholder="Cari" />
-                  <button
-                    className="float-left h-10 block border border-gray-500 rounded rounded-l-none bg-gray-200 border-l-0 w-10">
-                    <i className="lni lni-search-alt"></i></button>
-                  <div className="clear-both"></div>
+                  id="" type="text" placeholder="Cari"/>
+                <button
+                  className="float-left h-10 block border border-gray-500 rounded rounded-l-none bg-gray-200 border-l-0 w-10">
+                  <i className="lni lni-search-alt"></i></button>
+                <div className="clear-both"></div>
               </div>
             </div>
           </div>
           <div className="float-right flex items-center h-full px-2 py-1">
-            <div className="float-left flex flex-row items-center h-full border-l border-gray-600 p-4">
-              <span className="flex flex-row items-center justify-center h-10 w-10 rounded-full bg-gray-400 mr-2">
-                <i className="lni lni-user"></i>
-              </span>
-              <a href="#" className="display-block text-white">{fullName}</a>
-            </div>
-            <div className="float-left flex flex-row items-center h-full border-l border-gray-600 p-4">
-              <a href="#" onClick={logoutClicked} className="display-block text-white">Logout</a>
+            <div className="float-left relative flex flex-row items-center h-full border-l border-gray-600 p-4">
+              <a href="#" onClick={toggleProfile} className="flex flex-row items-center h-full">
+                <span className="flex flex-row items-center justify-center h-10 w-10 rounded-full bg-gray-400 mr-2">
+                  <i className="lni lni-user"></i>
+                </span>
+                <span className="display-block text-white">{fullName}</span>
+                <svg className="fill-current text-white h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+                </svg>
+              </a>
+              {showProfile ?
+                (<ul className="absolute text-gray-700" style={{ top: '60px', left: '0px', right: '0px' }}>
+                  <li>
+                    <a
+                       className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Test</a>
+                  </li>
+                  <li>
+                    <a onClick={logoutClicked}
+                       className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Logout</a>
+                  </li>
+                </ul>) : <span>&nbsp;</span>
+              }
             </div>
           </div>
           <div className="clear-both"></div>
